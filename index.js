09644742de9a0b52152c2496b2ffd590d602191e
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-25 14:50:02 +1000

    added info for on click events in javascript halfway to making popup for pokemon

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const pokedex = document.getElementById("pokedex")
 
-
-
 const fetchPokemon = () => {
     const promises = [];
     for (let i = 1; i <= 150; i++) {
-        if (i === 148) {
+        if (i === 131) {
+            promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/lapras`).then((response) => response.json()))
+        }
+        else if(i === 69){
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/bellsprout`).then((response) => response.json()))
+        }
+        else if(i === 148){
             promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/dragonair`).then((response) => response.json()))
         }
         else {
@@ -32,17 +36,47 @@ const fetchPokemon = () => {
                 spDefenceVal: data.stats[4].base_stat,
                 speedVal: data.stats[5].base_stat,
             }));
+        
            displayPokemon(pokemon);
     });
 };
 
 const displayPokemon = (pokemon) => {
-    console.log(pokemon)
     const pokemonli = pokemon.map ( 
-        (pokelist) => `<img src="${pokelist.image}"/>` 
+        (pokelist) => `<li class="pictures" onclick="selectPokemon(${pokelist.id})">
+        <img class="pokeImage" src="${pokelist.image}"/></li>` 
         ).join('');
 
     pokedex.innerHTML = pokemonli;
 };
 
+const selectPokemon = async (id) => {
+    // console.log(id)
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const response = await fetch(url)
+    const pokelist = await response.json()
+    displayPopup(pokelist)
+}
+
+
+const displayPopup = (pokelist) => {
+    const type = pokelist.types.map( type => type.type.name).join(", ");
+    // console.log(type)
+    const htmlpopUP = `
+    <div class="popup">
+        <button id="closeButton" onclick="closePopup()"> X </button>
+            <div class=pokeInfo>
+                <h1> ${pokelist.id}. ${pokelist.name}</h1>
+                <p>Type: ${type}</p>
+                <p>Stats</p>
+                <p>${pokelist.hp}, ${pokelist.hpVal}</p>
+                <p>${pokelist.attack}, ${pokelist.stats[1].base_stat}</p>
+                <p>${pokelist.defence}, ${pokelist.defenceVal}</p>
+                <p>${pokelist.spAttack}, ${pokelist.spAttackVal}</p>
+                <p>${pokelist.spDefence}, ${pokelist.spDefenceVal}</p>
+                <p>${pokelist.speed}, ${pokelist.speedVal}</p>
+            </div>
+    </div>`;
+    console.log(htmlpopUP)
+}
 fetchPokemon()
