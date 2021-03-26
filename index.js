73d8508603325b07ee31ac21f1d8616002a4c047
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-26 21:14:47 +1000

    popup is working now  just a couple more tweaks before finalising

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fetchPokemon = () => {
         if (i === 131) {
             promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/lapras`).then((response) => response.json()))
         }
-        else if(i === 69){
-        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/bellsprout`).then((response) => response.json()))
+        else if(i === 13){
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/weedle`).then((response) => response.json()))
         }
         else if(i === 148){
             promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/dragonair`).then((response) => response.json()))
@@ -19,22 +19,22 @@ const fetchPokemon = () => {
 
     Promise.all(promises).then((results) => {
             const pokemon = results.map((data) => ({
-                name: data.name,
+                // name: data.name,
                 id: data.id,
                 image: data.sprites.front_default,
-                type: data.types.map( (type) => type.type.name).join(", "),
-                hp: data.stats[0].stat.name,
-                attack: data.stats[1].stat.name,
-                defence: data.stats[2].stat.name,
-                spAttack: data.stats[3].stat.name,
-                spDefence: data.stats[4].stat.name,
-                speed: data.stats[5].stat.name,
-                hpVal: data.stats[0].base_stat,
-                attackVal: data.stats[1].base_stat,
-                defenceVal: data.stats[2].base_stat,
-                spAttackVal: data.stats[3].base_stat,
-                spDefenceVal: data.stats[4].base_stat,
-                speedVal: data.stats[5].base_stat,
+                // type: data.types.map( (type) => type.type.name).join(", "),
+                // hp: data.stats[0].stat.name,
+                // attack: data.stats[1].stat.name,
+                // defence: data.stats[2].stat.name,
+                // spAttack: data.stats[3].stat.name,
+                // spDefence: data.stats[4].stat.name,
+                // speed: data.stats[5].stat.name,
+                // hpVal: data.stats[0].base_stat,
+                // attackVal: data.stats[1].base_stat,
+                // defenceVal: data.stats[2].base_stat,
+                // spAttackVal: data.stats[3].base_stat,
+                // spDefenceVal: data.stats[4].base_stat,
+                // speedVal: data.stats[5].base_stat,
             }));
         
            displayPokemon(pokemon);
@@ -65,18 +65,25 @@ const displayPopup = (pokelist) => {
     const htmlpopUP = `
     <div class="popup">
         <button id="closeButton" onclick="closePopup()"> X </button>
-            <div class=pokeInfo>
+            <div class="pictures">
+                <img class="pokeImagePop" src="${pokelist.sprites.front_default}"/>
                 <h1> ${pokelist.id}. ${pokelist.name}</h1>
                 <p>Type: ${type}</p>
                 <p>Stats</p>
-                <p>${pokelist.hp}, ${pokelist.hpVal}</p>
-                <p>${pokelist.attack}, ${pokelist.stats[1].base_stat}</p>
-                <p>${pokelist.defence}, ${pokelist.defenceVal}</p>
-                <p>${pokelist.spAttack}, ${pokelist.spAttackVal}</p>
-                <p>${pokelist.spDefence}, ${pokelist.spDefenceVal}</p>
-                <p>${pokelist.speed}, ${pokelist.speedVal}</p>
+                <p class="hp">${pokelist.stats[0].stat.name}, ${pokelist.stats[0].base_stat} </p>
+                <p class="attack">${pokelist.stats[1].stat.name}, ${pokelist.stats[1].base_stat}</p>
+                <p class="defence">${pokelist.stats[2].stat.name}, ${pokelist.stats[2].base_stat}</p>
+                <p class="SpAttack">${pokelist.stats[3].stat.name}, ${pokelist.stats[3].base_stat}</p>
+                <p class="SpDefence">${pokelist.stats[4].stat.name}, ${pokelist.stats[4].base_stat}</p>
+                <p class="speed">${pokelist.stats[5].stat.name}, ${pokelist.stats[5].base_stat}</p>
             </div>
     </div>`;
+    pokedex.innerHTML = htmlpopUP + pokedex.innerHTML;
     console.log(htmlpopUP)
 }
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+}
+
 fetchPokemon()
